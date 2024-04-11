8ab5ab823d56b79bc2a4c89251d377b7bcbc8e68
CharlesPatriarca <charlespatriarcati@gmail.com>
2024-04-11 09:36:54 -0300

    Atualização

--- exercise019.py
+++ exercise019.py
@@ -1,8 +1,8 @@
 negativeNumbers = 0
-for x in range (11):
+for x in range (10):
     numbers = int(input('Digite 10 números: '))
     if numbers < 0:
-     negativeNumbers = negativeNumbers +1
+     negativeNumbers += 1
 
 print(negativeNumbers)
 

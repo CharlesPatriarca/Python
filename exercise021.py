001e9cc5228aa60609b014592c288480442041e9
CharlesPatriarca <charlespatriarcati@gmail.com>
2024-04-11 11:06:52 -0300

    Atualização

--- exercise021.py
+++ exercise021.py
@@ -1,7 +1,16 @@
-valores = 0
+soma = 0
+divisor = 0
 for g in range (3):
-    valores = int (input('Digite sua notas: ')) + valores
-#soma += valores
-#media = soma / 3
-media = valores/3
-print(media)
+    valores = int(input('Digite sua notas: ')) #+ valores
+    if valores >= 0 and valores <= 10:
+        soma += valores
+        divisor += 1
+
+        #media = valores//3
+if divisor != 0:
+    media = soma / divisor
+    print(media)
+else:
+    print('Reveja sua vida')
+
+

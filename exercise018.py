9ce959a88dbe54da3c7645216ab01a4938295a99
CharlesPatriarca <charlespatriarcati@gmail.com>
2024-04-11 09:15:16 -0300

    Atualização

--- exercise018.py
+++ exercise018.py
@@ -1,4 +1,4 @@
 number = int (input('Digite um número para contagem: '))
 
 for x in range (1, number+1, 1):
-    print (x)
+    print (x, end=' ')
---
+++ exercise019.py
@@ -0,0 +1,8 @@
+negativeNumbers = 0
+for x in range (11):
+    numbers = int(input('Digite 10 números: '))
+    if numbers < 0:
+     negativeNumbers = negativeNumbers +1
+
+print(negativeNumbers)
+

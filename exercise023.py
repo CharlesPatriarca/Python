d67e25d339a9561ee204258b57aa3e59a809ee86
CharlesPatriarca <charlespatriarcati@gmail.com>
2024-04-15 08:47:28 -0300

    Atualização

---
+++ exercise023.py
@@ -0,0 +1,9 @@
+valor1 = int( input('Digite o primeiro valor: '))
+valor2 = int (input('Digite o segundo valor: '))
+
+while valor2 == 0:
+    valor2 = int(input('Digite novamente, apenas valores diferente de zero: '))
+
+divisao = valor1 // valor2
+
+print(divisao)

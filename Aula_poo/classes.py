e89b22d3ee439aabd70af240fbeb92cd796538bf
CharlesPatriarca <charlespatriarcati@gmail.com>
2024-05-22 16:44:34 -0300

    Atualização

--- Aula_poo/classes.py
+++ Aula_poo/classes.py
@@ -1,20 +1,188 @@
 class Pessoa():
-    def __init__(self, nomeAluno, pesoAluno, idadeAluno, comendo = False):
+    def __init__(self, nomeAluno, pesoAluno, idadeAluno):
         self.nome = nomeAluno
         self.peso = pesoAluno
         self.idade = idadeAluno
+        self.comendo = False
+        self.falando = True
+        self.dormindo = True
+        self.pararComer = True
+        self.acordando = True
+        self.naoFale = False
 
     def comer(self, comida, bebida):
-        print (f'{self.nome} foi comer {comida} com {bebida}')
+        if self.comendo == True:
+            print(f'{self.nome} não pode falar e nem dormir, deve continuar comendo sua {comida} e tomando seu {bebida}')
+        else:
+            self.comendo == False
+            print(f'{self.nome} já pode falar ou dormir, teminou de comer.')
 
-    def falar(self, cafe, smartphone):
-        print (f'{self.nome} esta falando com {cafe} pelo {smartphone}')
+    def falar(self):
+        if  self.falando == True:
+            print(f'{self.nome} deve parar de falar para poder comer ou dormir')
+        else:
+            self.falando == False
+            print (f'{self.nome} pode ir dormir ou comer, pois parou de falar')
 
-    def dormir(self, horario):
-        print (f'{self.nome} dormiu às {horario}')
+    def dormir(self):
+        if self.dormindo == True:
+            print(f'{self.nome} está dormindo, não incomode ele.')
+        else:
+            self.comendo == False
+            print (f'{self.nome} pode comer ou falar, porque não está dormindo')
 
-    # def pareComer(self):
-    #
-    # def acordar(self):
-    #
-    # def pararFalar(self):
+    def pareComer(self, comida, bebida):
+        if self.pararComer == True:
+            print (f'{self.nome} você deve parar de comer a {comida} e o {bebida}')
+        else:
+            print (f'{self.nome} pode continuar comendo')
+    def acordar(self):
+        if self.acordando == True:
+            print (f'{self.nome} acorde parar comer.')
+        else:
+            self.comendo == False
+            print (f'{self.nome} já ta gordinho, não precisa acordar pra comer')
+
+    def pararFalar(self):
+        if self.naoFale == True:
+            print (f'{self.nome} ninguém quer ouvir você não fale mais')
+        else:
+            self.naoFale == False
+            print (f'{self.nome} pode começar a falar')
+
+
+class Banco():
+
+    def __init__(self, clienteNome, numeroConta, tipoConta):
+        self.clienteNome = clienteNome
+        self.numeroConta = numeroConta
+        self.tipoConta = tipoConta
+        self.saldo = 0.00
+        self.statusConta = False
+
+    def ativarConta(self):
+        if self.statusConta == False:
+            self.statusConta = True
+            print('Conta ativada com sucesso')
+        else:
+            print('Conta já está ativada')
+
+    def desativarConta(self):
+        if self.statusConta == True:
+            if self.saldo == 0:
+                self.statusConta = False
+                print('Conta desativada com sucesso')
+            else:
+                print('Não é possível desativar, ainda possui saldo')
+        else:
+            print('Conta já está desativada')
+
+    def depositar(self):
+        if self.statusConta == False:
+            print('Sua conta está desativada, não pode depositar')
+            self.statusConta = True
+        else:
+            print('Pode depositar')
+
+    def sacar(self, saldo):
+        if saldo > self.saldo:
+            print('Pode sacar seu dinheiro')
+        else:
+            print('Saldo zerado, não pode sacar')
+
+
+class Animal():
+    def __init__(self, nome, cor):
+        self.nome = nome
+        self.cor = cor
+
+    def comer(self):
+        print(f'{self.nome} sem comida hoje')
+
+
+class Gato(Animal):
+    def __init__(self, nome, cor):
+        super().__init__(nome, cor)
+
+    def miar(self):
+        print(f'O {self.nome} está miando')
+
+    def latir1(self):
+        print('Seu gato esta possesso, ele está latindo')
+
+
+class Cachorro(Animal):
+    def __init__(self, nome, cor):
+        super().__init__(nome, cor)
+
+    def latir(self):
+        print(f'O {self.nome} está latindo')
+
+
+class Coelho(Animal):
+    def __init__(self, nome, cor):
+        super().__init__(nome, cor)
+
+    def roer(self):
+        print(f'O {self.nome} está roendo')
+
+
+class Vaca(Animal):
+    def __init__(self, nome, cor):
+        super().__init__(nome, cor)
+
+    def muje(self):
+        print(f'O {self.nome} está mujindo')
+
+
+class Forma():
+    def __init__(self):
+        self.area = 0
+        self.perimetro = 0
+
+
+class Retangulo(Forma):
+    def __init__(selfo):
+        super().__init__()
+
+    def calcularArea(self, base, altura):
+        self.area = base * altura
+        print(f'A área do retângulo é {self.area}')
+
+    def calcularPerimetro(self, base, altura):
+        self.perimetro = (base + altura) * 2
+
+        print(f'O perímetro do retângulo é {self.perimetro}')
+
+
+class Triangulo(Forma):
+    def __init__(self):
+        super().__init__()
+
+    def areaTriangulo(self, base, altura):
+        self.area = (base * altura) // 2
+        print(f'A area do triangulo é {self.area}')
+
+    def perimetroTriangulo(self, l1, l2, l3):
+        self.perimetro = l1 + l2 + l3
+        print(f'Perimetro igual a {self.perimetro}')
+
+
+class Atleta():
+    def __init__(self):
+        self.aposentado = True
+        self.peso = 52
+
+    def aposentar(self):
+        if self.aposentado == True:
+            print(f'Atleta já está aposentado')
+        else:
+            print(f'Atleta ainda pode competir')
+
+    def peso(self):
+        print(f'O peso do atleta ')
+
+
+class Corredor(Atleta):
+    def __init__(self):
+        super().__init__()
--- Aula_poo/main.py
+++ Aula_poo/main.py
@@ -1,5 +1,2 @@
 from classes import *
 
-p1 = Pessoa ('Charles', 56, 23)
-p2 = Pessoa ('Isabelly', 45, 22)
-
